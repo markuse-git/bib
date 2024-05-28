5b5efd6a18b9d520c5c10ecb13863b7bd6f4c99d
Markus <markus.eichelhardt@web.de>
2024-05-28 21:15:08 +0200

    with user input

--- main.cc
+++ main.cc
@@ -3,29 +3,95 @@
 #include "book.h"
 #include "user.h"
 
+
+void print_menue(){
+    std::cout << "1. Add User" << std::endl;
+    std::cout << "2. Add Book" << std::endl;
+    std::cout << "3. Borrow Book" << std::endl;
+    std::cout << "4. Return Book" << std::endl;
+    std::cout << "5. Remove User" << std::endl;
+    std::cout << "6. Remove Book" << std::endl;
+    std::cout << "7. Exit" << std::endl;
+}
+
+
 int main() {
+
     Library library("library.db");
+    int choice;
 
-    // Benutzer hinzufügen
-    User user2(2, "Hans Hartz", "hans@hartz.de");
-    library.add_user(user2);
+    while(true){
+        print_menue();
+        std::cout << "Enter your choice" << std::endl;
+        std::cin >> choice;
 
-    // Bücher hinzufügen
-    Book book2(2, "second book", "Ursula Pitchi", "23456789");
-    library.add_book(book2);
+        if(choice == 7){
+            break;
+        }
 
-    // Buch ausleihen
-    // library.borrow_book(2, 2);
+        int id;
+        std::string name, email, title, author, isbn;
 
-    // Buch zurückgeben
-    library.return_book(2, 2);
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Enter User Id: " << std::endl;
+            std::cin >> id;
+            std::cout << "Enter user name: " << std::endl;
+            std::cin.ignore();
+            std::getline(std::cin, name);
+            std::cout << "Enter User email: " << std::endl;
+            std::getline(std::cin, email);
+            library.add_user(User(id, name, email));
+            break;
 
-    // Benutzer entfernen
-    // library.remove_user(1);
+        case 2:
+            std::cout << "Enter Book Id: " << std::endl;
+            std::cin >> id;
+            std::cout << "Enter book title: " << std::endl;
+            std::cin.ignore();
+            std::getline(std::cin, title);
+            std::cout << "Enter Book Author: " << std::endl;
+            std::getline(std::cin, author);
+            std::cout << "Enter Book ISBN" << std::endl;
+            std::getline(std::cin, isbn);
+            library.add_book(Book(id, title, author, isbn));
+            break;
 
-    // Buch entfernen
-    // library.remove_book(1);
+        case 3:
+            std::cout << "Enter User Id: " << std::endl;
+            std::cin >> id;
+            std::cout << "Enter Book ID: " << std::endl;
+            int book_id;
+            std::cin >> book_id;
+            library.borrow_book(book_id, id);
+            break;
 
-    std::cout << "Program finished successfully" << std::endl;
+        case 4:
+            std::cout << "Enter User Id: " << std::endl;
+            std::cin >> id;
+            std::cout << "Enter Book ID: " << std::endl;
+            std::cin >> book_id;
+            library.return_book(book_id, id);
+            break;
+
+        case 5:
+            std::cout << "Enter User Id: " << std::endl;
+            std::cin >> id;
+            library.remove_user(id);
+            break;
+
+        case 6:
+            std::cout << "Enter Book Id: " << std::endl;
+            std::cin >> id;
+            library.remove_book(id);
+            break;
+        
+        default:
+            std::cout << "Invalid choice, please try again." << std::endl;
+            break;
+        }
+    }
+
     return 0;
 }
